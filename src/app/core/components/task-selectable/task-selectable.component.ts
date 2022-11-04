5502bcacebf659768bbc75b360a4305ee6ba2f19
Anfisbena <santos.lopez.alu@iescampanillas.com>
2022-11-04 10:47:16 +0100

    allá va, a comenzar los selectables.

---
+++ src/app/core/components/task-selectable/task-selectable.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-selectable',
+  templateUrl: './task-selectable.component.html',
+  styleUrls: ['./task-selectable.component.scss'],
+})
+export class TaskSelectableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
